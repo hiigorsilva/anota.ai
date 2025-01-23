dac2d80bad048bf9eecd431805b21bbc5bd95c41
hiigorsilva <higorscontato@gmail.com>
2025-01-23 15:25:17 -0300

    refactor: remove avatar image and add menu icon on profile button

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,20 +1,13 @@
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
+import { MenuIcon } from 'lucide-react'
 import { ContainerApp } from '../container-app'
 import { Search } from '../search'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
 import { ProfileButtonSheet } from './profile-button-sheet'
 import { TeamSwitcher } from './team-switcher'
 
 export const Navbar = async () => {
-  const user = await currentUser()
-  if (!user) {
-    redirect('/sign-in')
-  }
-
   return (
-    <header className="w-full h-16 border-b">
+    <header className="flex items-center min-h-16 h-fit w-full">
       <ContainerApp className="flex justify-between items-center gap-8">
         <TeamSwitcher />
 
@@ -23,17 +16,8 @@ export const Navbar = async () => {
 
           {/* PROFILE BUTTON */}
           <ProfileButtonSheet>
-            <Button variant="ghost">
-              <Avatar className="size-8">
-                <AvatarImage
-                  src={
-                    user.imageUrl ||
-                    `http://avatar.vercel.sh/${user.firstName}.png`
-                  }
-                  alt={user.fullName || 'Foto do usuário'}
-                />
-                <AvatarFallback>{user.firstName?.charAt(0)}</AvatarFallback>
-              </Avatar>
+            <Button variant="outline" size="icon">
+              <MenuIcon className="size-6 shrink-0" />
             </Button>
           </ProfileButtonSheet>
         </div>
