49f15cf875bafe474bdd83f02a4e2223a769d02c
hiigorsilva <higorscontato@gmail.com>
2025-01-27 16:02:15 -0300

    feat: add revalidatePath when create task

--- src/actions/task/index.ts
+++ src/actions/task/index.ts
@@ -3,6 +3,7 @@
 import type { TaskFormType } from '@/schemas/task-form-schema'
 import { createTaskService } from '@/services/task'
 import { currentUser } from '@clerk/nextjs/server'
+import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 export const createTaskAction = async (task: TaskFormType) => {
@@ -12,4 +13,7 @@ export const createTaskAction = async (task: TaskFormType) => {
   }
 
   await createTaskService(task)
+
+  revalidatePath('/')
+  revalidatePath('/tasks')
 }
