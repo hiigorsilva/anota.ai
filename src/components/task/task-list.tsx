4559a8c7a8ebdc7f2e35f21c5a4c9ddecbc9edaa
hiigorsilva <higorscontato@gmail.com>
2025-01-29 16:31:02 -0300

    feat: add task details modal

--- src/components/task/task-list.tsx
+++ src/components/task/task-list.tsx
@@ -1,6 +1,17 @@
 import { getTaskAction } from '@/actions/task/get-task'
 import { getStatusColor } from '@/data/task/status-options'
 import { formatDate } from '@/utils/date-format'
+import { PencilIcon } from 'lucide-react'
+import { Button } from '../ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog'
+import { Separator } from '../ui/separator'
 import {
   Table,
   TableBody,
@@ -19,9 +30,12 @@ export const TaskList = async () => {
       {!tasks && <TableCaption>Nenhuma tarefa cadastrada.</TableCaption>}
       <TableHeader>
         <TableRow>
-          <TableHead>Data</TableHead>
-          <TableHead>Título da tarefa</TableHead>
-          <TableHead>Status</TableHead>
+          <TableHead className="w-36 text-nowrap">Data</TableHead>
+          <TableHead className="min-h-64 flex-1 text-nowrap">
+            Título da tarefa
+          </TableHead>
+          <TableHead className="w-40 text-nowrap">Status</TableHead>
+          <TableHead className="w-10 text-nowrap">{''}</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -29,23 +43,50 @@ export const TaskList = async () => {
           tasks.map(task => (
             <TableRow
               key={task.id}
-              className={
+              className={`text-nowrap ${
                 task.status === 'Cancelado' || task.status === 'Concluído'
                   ? 'text-muted-foreground'
                   : ''
-              }
+              }`}
             >
               <TableCell className="capitalize">
                 {formatDate(task.createdAt)}
               </TableCell>
-              <TableCell
-                className={
-                  task.status === 'Cancelado' || task.status === 'Concluído'
-                    ? 'line-through'
-                    : ''
-                }
-              >
-                {task.title}
+              <TableCell className="min-h-64 text-nowrap">
+                <Dialog>
+                  <DialogTrigger
+                    className={
+                      task.status === 'Cancelado' || task.status === 'Concluído'
+                        ? 'line-through'
+                        : ''
+                    }
+                  >
+                    {task.title}
+                  </DialogTrigger>
+                  <DialogContent className="flex flex-col gap-6">
+                    <DialogHeader className="space-y-3">
+                      <DialogTitle>{task.title}</DialogTitle>
+                      <DialogDescription className="flex justify-between items-center gap-6">
+                        <span>Atualizado em {formatDate(task.updatedAt)}</span>
+                        <span className="flex items-center gap-2">
+                          <div
+                            className="size-2.5 rounded-full"
+                            style={{
+                              backgroundColor: getStatusColor(task.status),
+                            }}
+                          />
+                          {task.status}
+                        </span>
+                      </DialogDescription>
+                    </DialogHeader>
+                    <Separator />
+                    <div>
+                      <p className="text-muted-foreground">
+                        {task.description}
+                      </p>
+                    </div>
+                  </DialogContent>
+                </Dialog>
               </TableCell>
               <TableCell>
                 <div className="flex items-center gap-2">
@@ -55,6 +96,12 @@ export const TaskList = async () => {
                   />
                   {task.status}
                 </div>
+              </TableCell>
+
+              <TableCell>
+                <Button size="icon" variant="ghost">
+                  <PencilIcon className="size-4 shrink-0" />
+                </Button>
               </TableCell>
             </TableRow>
           ))}
