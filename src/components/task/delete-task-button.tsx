1adc22d3d0325b70b5ac7c5cbc981780c10c9076
hiigorsilva <higorscontato@gmail.com>
2025-01-30 18:01:41 -0300

    refactor: add confirmation modal to remove task

--- src/components/task/delete-task-button.tsx
+++ src/components/task/delete-task-button.tsx
@@ -3,20 +3,59 @@
 import { removeTaskAction } from '@/actions/task/remove-task'
 import type { TaskType } from '@/schemas/list-task'
 import { Trash2Icon } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'sonner'
 import { Button } from '../ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '../ui/dialog'
 
 type Props = {
   task: TaskType
 }
 
 export const DeleteTaskButton = ({ task }: Props) => {
+  const [open, setOpen] = useState(false)
+
+  const handleRemoveTask = async () => {
+    try {
+      removeTaskAction(task.id)
+      toast.success('Tarefa removida com sucesso')
+    } catch (err) {
+      toast.error('Erro ao remover tarefa')
+      console.error('❌ Erro ao remover tarefa: ', err)
+    } finally {
+      setOpen(false)
+    }
+  }
+
   return (
-    <Button
-      size="icon"
-      variant="ghost"
-      onClick={() => removeTaskAction(task.id)}
-    >
-      <Trash2Icon className="text-rose-600 dark:text-rose-500 size-4 shrink-0" />
-    </Button>
+    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+      <DialogTrigger asChild>
+        <Button size="icon" variant="ghost">
+          <Trash2Icon className="text-rose-600 dark:text-rose-500 size-4 shrink-0" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Tem certeza que deseja remover essa tarefa?</DialogTitle>
+          <DialogDescription>Essa ação não pode ser desfeita</DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="secondary">Cancelar</Button>
+          </DialogClose>
+          <Button onClick={handleRemoveTask}>Remover</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
