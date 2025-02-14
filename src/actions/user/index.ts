b0a4000e4bf0471dcf67819e28e45e5014ed8c5b
hiigorsilva <higorscontato@gmail.com>
2025-02-14 16:41:25 -0300

    feat: add get current user

--- src/actions/user/index.ts
+++ src/actions/user/index.ts
@@ -1,18 +1,17 @@
 'use server'
 
-import { auth } from '@/auth'
+import { currentUser } from '@/services/db/user'
 import { redirect } from 'next/navigation'
+import { sessionUser } from '../auth'
 
-export const getUserSession = async () => {
-  const session = await auth()
-  if (!session) {
+export const getUserAction = async () => {
+  const session = await sessionUser()
+  if (!session.id) {
     redirect('/sign-in')
   }
 
-  const user = session.user
-  if (!user) {
-    redirect('/sign-in')
-  }
+  const user = await currentUser(session.id)
+  if (!user) return null
 
-  return { user }
+  return user
 }
---
+++ src/services/db/user.ts
@@ -0,0 +1,14 @@
+import { db } from '@/lib/db'
+
+export const currentUser = async (userId: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id: userId },
+    })
+
+    return user
+  } catch (err) {
+    console.error('❌ FAIL_TO_GET_USER_BY_EMAIL: ', err)
+    return null
+  }
+}
