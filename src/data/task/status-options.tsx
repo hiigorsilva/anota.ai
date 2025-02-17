76171a1824caa8ccc780e41b17f825004f701653
hiigorsilva <higorscontato@gmail.com>
2025-02-17 17:39:49 -0300

    style: set new status colors

--- src/data/task/status-options.tsx
+++ src/data/task/status-options.tsx
@@ -1,11 +1,11 @@
 export const statusOptions = [
-  { label: 'Pendente', color: '#9CA3AF' },
-  { label: 'Fazendo', color: '#FACC15' },
-  { label: 'Concluído', color: '#4ADE80' },
-  { label: 'Cancelado', color: '#FB7185' },
+  { label: 'Pendente', color: '#ffffff' },
+  { label: 'Fazendo', color: '#0ea5e9' },
+  { label: 'Concluído', color: '#16a34a' },
+  { label: 'Cancelado', color: '#ef4444' },
 ]
 
 export const getStatusColor = (label: string): string => {
   const status = statusOptions.find(status => status.label === label)
-  return status ? status.color : '#ffffff' // Cor padrão se não encontrar o status
+  return status ? status.color : '#9ca3af' // Cor padrão se não encontrar o status
 }
