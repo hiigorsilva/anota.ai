592d5d29bb51a151285888faf4ae9d721e1d1cfe
hiigorsilva <higorscontato@gmail.com>
2025-02-17 15:23:39 -0300

    refactor:  use task list title component

--- src/components/task/task-list-table.tsx
+++ src/components/task/task-list-table.tsx
@@ -1,22 +1,10 @@
-import { getStatusColor } from '@/data/task/status-options'
 import { formatTime } from '@/utils/format-time'
 import type { Task } from '@prisma/client'
-import { NotebookPenIcon } from 'lucide-react'
 import { Button } from '../ui/button'
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '../ui/dialog'
-import { Separator } from '../ui/separator'
+import { DialogClose } from '../ui/dialog'
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -25,6 +13,7 @@ import {
 import { DeleteTaskButton } from './delete-task-button'
 import { EditTaskDialog } from './edit-task-dialog'
 import { StatusLabel } from './task-status-label'
+import { TaskTitleModal } from './task-title-modal'
 
 type Props = {
   tasks: Task[]
@@ -33,16 +22,6 @@ type Props = {
 export const TaskListTable = async ({ tasks }: Props) => {
   return (
     <Table>
-      {/* TASKS NOT FOUND */}
-      {!tasks && (
-        <TableCaption className="py-8">
-          <NotebookPenIcon
-            strokeWidth={1.5}
-            className="text-muted-foreground mb-2 size-6"
-          />
-          Nenhuma tarefa cadastrada.
-        </TableCaption>
-      )}
       <TableHeader>
         <TableRow>
           <TableHead className="w-36 text-nowrap">Adicionado</TableHead>
@@ -55,79 +34,40 @@ export const TaskListTable = async ({ tasks }: Props) => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {tasks &&
-          tasks.map(task => (
-            <TableRow
-              key={task.id}
-              className={`text-nowrap ${
-                task.status === 'Cancelado' || task.status === 'Concluído'
-                  ? 'text-muted-foreground'
-                  : ''
-              }`}
-            >
-              <TableCell className="capitalize">
-                {formatTime(task.createdAt)}
-              </TableCell>
-              <TableCell className="min-h-64 text-nowrap">
-                <Dialog>
-                  {/* TODO: Resolver BUG ao clicar no título / abrir modal de edição */}
-                  <DialogTrigger
-                    className={`hover:underline ${
-                      task.status === 'Cancelado'
-                        ? 'text-red-500 line-through'
-                        : ''
-                    }`}
-                  >
-                    {task.title}
-                  </DialogTrigger>
-                  <DialogContent className="flex flex-col gap-6">
-                    <DialogHeader className="space-y-3">
-                      <DialogTitle>{task.title}</DialogTitle>
-                      <DialogDescription className="flex justify-between items-center gap-6">
-                        <span className="text-sm">
-                          Atualizado {formatTime(task.updatedAt)}
-                        </span>
-                        <div className="flex items-center gap-2">
-                          <div
-                            className="size-2.5 rounded-full"
-                            style={{
-                              backgroundColor: getStatusColor(task.status),
-                            }}
-                          />
-                          {task.status}
-                        </div>
-                      </DialogDescription>
-                    </DialogHeader>
-                    <Separator />
-                    <div>
-                      <p className="text-sm text-muted-foreground">
-                        {task.description}
-                      </p>
-                    </div>
-                  </DialogContent>
-                </Dialog>
-              </TableCell>
-              <TableCell>
-                <StatusLabel status={task.status} />
-              </TableCell>
-
-              <TableCell className="lowercase">
-                {formatTime(task.updatedAt)}
-              </TableCell>
-
-              <TableCell>
-                <EditTaskDialog currentTask={task}>
-                  <DialogClose asChild>
-                    <Button variant="secondary" className="w-full">
-                      Cancelar
-                    </Button>
-                  </DialogClose>
-                </EditTaskDialog>
-                <DeleteTaskButton task={task} />
-              </TableCell>
-            </TableRow>
-          ))}
+        {tasks && tasks.map(task => <TaskTableRow key={task.id} task={task} />)}
       </TableBody>
     </Table>
+  )
+}
+
+const TaskTableRow = ({ task }: { task: Task }) => {
+  const colorLabelRow =
+    task.status === 'Cancelado' || task.status === 'Concluído'
+      ? 'text-muted-foreground'
+      : ''
+
+  return (
+    <TableRow key={task.id} className={`text-nowrap ${colorLabelRow}`}>
+      <TableCell className="capitalize">{formatTime(task.createdAt)}</TableCell>
+      <TableCell className="min-h-64 text-nowrap">
+        <TaskTitleModal task={task} />
+      </TableCell>
+      <TableCell>
+        <StatusLabel status={task.status} />
+      </TableCell>
+
+      <TableCell className="lowercase">{formatTime(task.updatedAt)}</TableCell>
+
+      <TableCell>
+        <EditTaskDialog currentTask={task}>
+          <DialogClose asChild>
+            <Button variant="secondary" className="w-full">
+              Cancelar
+            </Button>
+          </DialogClose>
+        </EditTaskDialog>
+        <DeleteTaskButton task={task} />
+      </TableCell>
+    </TableRow>
   )
 }
