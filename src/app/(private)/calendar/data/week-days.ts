ffe4aaa94e37dcb1d4fa9b01976e2f4ae424c119
hiigorsilva <higorscontato@gmail.com>
2025-03-06 15:51:35 -0300

    feat: add week days to calendar component

---
+++ src/app/(private)/calendar/data/week-days.ts
@@ -0,0 +1,11 @@
+const WEEK_DAYS = {
+  'en-US': {
+    daysOfWeek: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  },
+  'pt-BR': {
+    daysOfWeek: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+  },
+}
+
+const lang = 'pt-BR'
+export const DAYS_OF_WEEK = WEEK_DAYS[lang].daysOfWeek
