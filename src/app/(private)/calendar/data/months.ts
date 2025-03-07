42f007f92a5743156c276f53b9a71c90b7d60d11
hiigorsilva <higorscontato@gmail.com>
2025-03-07 04:10:22 -0300

    feat: add months list in pt-BR

---
+++ src/app/(private)/calendar/data/months.ts
@@ -0,0 +1,37 @@
+const MONTHS = {
+  'en-US': {
+    monthsOfYear: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+  },
+  'pt-BR': {
+    monthsOfYear: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro',
+    ],
+  },
+}
+
+const lang = 'pt-BR'
+export const MONTHS_OF_YEAR = MONTHS[lang].monthsOfYear
