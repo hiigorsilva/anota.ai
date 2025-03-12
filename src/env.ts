a13f065bc6fe986cb6ad2b9489cc13dee47bbdc0
hiigorsilva <higorscontato@gmail.com>
2025-03-12 14:56:55 -0300

    fix: remove AUTH_SECRET that was not used

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  AUTH_SECRET: z.string(),
   AUTH_GOOGLE_ID: z.string(),
   AUTH_GOOGLE_SECRET: z.string(),
   DATABASE_URL: z.string().url(),
